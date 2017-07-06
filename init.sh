d21c02cc7493c58a588f30f2ca51f7b04693c98e
Morten Scheel <ms@findforsikring.dk>
2017-07-06 16:15:53 +0200

    Updated init script

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-rm -rf .git
+rm -rf .git # Remove git repo after cloning
 if [ ! -f './.env' ]; then
-    cp ./.env.example ./.env
-    echo "Copied .env"
+    cp ./.env.example ./.env > /dev/null
+    echo "Copied .env file"
 fi
-composer install
+composer install > /dev/null
+echo "Installed composer packages"
 php artisan key:generate
-touch ./database/database.sqlite
+touch ./database/database.sqlite > /dev/null
+echo "Added sqlite DB"
 php artisan migrate --seed
